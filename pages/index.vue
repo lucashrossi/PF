8e8658a98f12dfa189ac6b7c87ec670b13e0f3f1
lucashrossi <lucashrossi@gmail.com>
2018-09-25 18:01:34 -0300

    PF

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,13 @@
       app
       temporary
     >
-      <v-list dense >
-        <v-list-tile >
+      <v-list >
+        <v-list-tile v-for="D in DD" :key="D.title">
           <v-list-tile-action>
-            <v-icon>home</v-icon>
+            <v-icon>{{D.icon}}</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Home</v-list-tile-title>
+            <v-list-tile-title>{{D.title}}</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         
@@ -21,12 +21,28 @@
 
     <v-toolbar color="blue-grey darken-3" dark fixed app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-sm-and-up"></v-toolbar-side-icon>
-      <!-- <v-toolbar-title>DashBoard</v-toolbar-title>
-      <v-spacer></v-spacer> -->
+      <v-toolbar-title>
+         <v-btn flat>
+            <v-icon left>home</v-icon>
+            DashBoard
+        </v-btn>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
         <v-btn flat>
-            <v-icon left>home</v-icon>
-            Home
+            <v-icon left>trending_up</v-icon>
+            Performance
+        </v-btn>
+         <v-btn flat>
+            <v-icon left>schedule</v-icon>
+            Candidatas
+        </v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer> 
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+            <v-icon left>invert_colors</v-icon>
+            Color
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
@@ -50,7 +66,12 @@
 <script>
   export default {
     data: () => ({
-      drawer: false
+      drawer: false,
+      DD: [
+          {icon: 'home', title: 'Dashboard'},
+          {icon: 'trending_up', title: 'Performance'},
+          {icon: 'schedule', title: 'Candidatas'}
+          ]
     }),
     props: {
       source: String
