4c0552b24810b06f25b377b400c6a0b3531dbdbb
Hernán <lucashrossi@gmail.com>
2018-10-02 21:24:11 -0300

    PF

--- pages/Fiscalizacion/index.vue
+++ pages/Fiscalizacion/index.vue
@@ -50,30 +50,7 @@
       <br>
       <br>
 
-      <v-layout row wrap>
-        <v-flex xs5> <!--class="text-xs-center">-->
-          <BU/>
-        </v-flex>
-        <v-spacer></v-spacer> 
-        <v-flex xs5> <!--class="text-xs-center">-->
-          <MIX/>
-        </v-flex>
-      </v-layout>
-
-      <br>
-      <br>
-      <br>
-      <br>
-
-      <v-layout row wrap>
-        <v-flex xs5> <!--class="text-xs-center">-->
-          <DEJ/>
-        </v-flex>
-        <v-spacer></v-spacer> 
-        <v-flex xs5> <!--class="text-xs-center">-->
-          
-        </v-flex>
-      </v-layout>
+      
 
     </v-container>
   </div>
--- pages/Ventas/index.vue
+++ pages/Ventas/index.vue
@@ -1,5 +1,34 @@
 <template>
-  <div></div>
+  <div>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs5> <!--class="text-xs-center">-->
+          <BU/>
+        </v-flex>
+        <v-spacer></v-spacer> 
+        <v-flex xs5> <!--class="text-xs-center">-->
+          <MIX/>
+        </v-flex>
+      </v-layout>
+
+      <br>
+      <br>
+      <br>
+      <br>
+
+      <v-layout row wrap>
+        <v-flex xs5> <!--class="text-xs-center">-->
+          <DEJ/>
+        </v-flex>
+        <v-spacer></v-spacer> 
+        <v-flex xs5> <!--class="text-xs-center">-->
+          
+        </v-flex>
+      </v-layout>
+
+
+    </v-container>
+  </div>
 </template>
 
 <script>
