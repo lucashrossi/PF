1762ba8dbbcbe49db4a72ceb497a8d1de4bfdeac
Hernán <lucashrossi@gmail.com>
2018-10-05 23:04:12 -0300

    PF

--- plugins/core-components.js
+++ plugins/core-components.js
@@ -3,9 +3,6 @@ import Vue from 'vue'
 import NC from '~/components/NC.vue'
 import NCC from '~/components/NCC.vue'
 // import pdf from 'vue-pdf'
-// import VuePDFViewer from 'vue-pdf-viewer'
-// import pdfshower from 'vue-pdf-shower'
-// import pdf from 'pdfvuer'
 import tabla from '~/components/tabla.vue'
 import FISC from '~/components/FISC.vue'
 import PBF from '~/components/PBF.vue'
@@ -22,8 +19,6 @@ import HOR from '~/components/HOR.vue'
 Vue.component('NC', NC)
 Vue.component('NCC', NCC)
 // Vue.component('pdf', pdf)
-// Vue.component('VuePDFViewer', VuePDFViewer)
-// Vue.component('pdfshower', pdfshower)
 Vue.component('tabla', tabla)
 Vue.component('FISC', FISC)
 Vue.component('PBF', PBF)
