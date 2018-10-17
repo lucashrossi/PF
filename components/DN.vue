58c4ca78bbf44d3dcd1cd3f3f04647e2e0c4bf6d
Hernán <lucashrossi@gmail.com>
2018-10-16 22:34:39 -0300

    PF

--- components/DN.vue
+++ components/DN.vue
@@ -7,12 +7,12 @@ export default {
     data () {
         return {
         datacollection: {
-            labels: ['BUENOS AIRES', 'CAPITAL', 'CORDOBA', 'SANTA FE', 'LA PAMPA', 'ENTRE RIOS', 'TUCUMAN', 'SALTA', 'S. DEL ESTERO', 'JUJUY', 'CHACO'],
+            labels: ['BUENOS AIRES', 'CAPITAL', 'SANTA FE', 'CORDOBA', 'ENTRE RIOS', 'TUCUMAN', 'S. DEL ESTERO', 'LA PAMPA', 'SALTA', 'JUJUY', 'CHACO'],
             datasets: [
                 {
                 label: 'Buck',
-                data: [84690, 34893, 13903, 28179, 1983, 4319, 2777, 1312, 2528, 440, 4],
-                backgroundColor: ['rgba(255,0,0)', 'rgba(50,200,0)','#3cba9f','#e8c3b9','#c45850'],
+                data: [84690, 34893, 28179, 13903, 4319, 2777, 2528, 1983, 1312, 440, 4],
+                backgroundColor: ['rgba(255,0,0)', 'rgba(50,200,0)','#3cba9f','#e8c3b9','#c45850','rgba(20,50,0)', 'rgba(20,100,20)','#38ba93','#e1c8b9','#c25250'],
                 // borderColor: ['rgba(255,0,0,0)', 'rgba(50,200,0,0)', 'rgba(0,0,255,0.9)'],
                 borderWidth: [0,0,0,0,0,0,0,0,0,0,0]
                 },
--- components/HOR.vue
+++ components/HOR.vue
@@ -9,96 +9,96 @@ export default {
         datacollection: {
             labels: ['SUDESTE', 'SUDOESTE', 'NORTE'],
             datasets: [
-            {
-                label: '75 ANIVERSARIO',
-                backgroundColor: '#f87979',
-                data: [71, 0, 0]
-            },
-            {
-                label: 'BAQUEANO',
-                backgroundColor: '#7979f8',
-                data: [108, 286, 0]
-            },
-            {
-                label: 'CHACARERO',
-                backgroundColor: '#79f879',
-                data: [30, 0, 0]
-            },
-            {
-                label: 'MALEVO',
-                backgroundColor: '#f8f379',
-                data: [158, 1240, 0]
-            },
-            {
+                {
                 label: 'METEORO',
                 backgroundColor: '#35645a',
                 data: [17160, 12741, 1264]
-            },
-            {
-                label: 'PLENO',
-                backgroundColor: '#925193',
-                data: [1110, 307, 1143]
-            },
-            {
-                label: 'RANQUEL',
-                backgroundColor: '#501313',
-                data: [225, 3029, 0]
-            },
-            {
-                label: 'BELLACO',
-                backgroundColor: '#f01313',
-                data: [0, 52, 0]
-            },
-            {
-                label: 'SAETA',
-                backgroundColor: '#485379',
-                data: [0, 90, 0]
-            },
-            {
-                label: '55 CL2',
-                backgroundColor: '#100000',
-                data: [0, 1027, 0]
-            },
-            {
-                label: 'TILCARA',
-                backgroundColor: '#f87979',
-                data: [200, 0, 0]
-            },
-            {
-                label: 'TAITA',
-                backgroundColor: '#251313',
-                data: [10, 0, 338]
-            },
-            {
+                },
+                {
                 label: 'SY 100',
                 backgroundColor: '#801313',
                 data: [8108, 4589, 4203]
-            },
-            {
-                label: 'SY 110',
-                backgroundColor: '#f81379',
-                data: [2770, 560, 576]
-            },
-            {
-                label: 'SY 200',
-                backgroundColor: '#797918',
-                data: [4771, 603, 1306]
-            },
-            {
+                },
+                {
                 label: 'SY 300',
                 backgroundColor: '#f87979',
                 data: [2802, 430, 7199]
-            },
-            {
-                label: 'SY 330',
-                backgroundColor: '#805050',
-                data: [0, 0, 110]
-            },
-            {
+                },
+                {
+                label: 'SY 200',
+                backgroundColor: '#797918',
+                data: [4771, 603, 1306]
+                },
+                {
+                label: 'SY 110',
+                backgroundColor: '#f81379',
+                data: [2770, 560, 576]
+                },
+                {
+                label: 'RANQUEL',
+                backgroundColor: '#501313',
+                data: [225, 3029, 0]
+                },
+                {
+                label: 'PLENO',
+                backgroundColor: '#925193',
+                data: [1110, 307, 1143]
+                },
+                {
+                label: 'MALEVO',
+                backgroundColor: '#f8f379',
+                data: [158, 1240, 0]
+                },
+                {
+                label: '55 CL2',
+                backgroundColor: '#100000',
+                data: [0, 1027, 0]
+                },
+                {
+                label: 'BAQUEANO',
+                backgroundColor: '#7979f8',
+                data: [108, 286, 0]
+                },
+                {
                 label: 'SY 015',
                 backgroundColor: '',
                 data: [150, 215, 0]
-            }
+                },
+                {
+                label: 'TAITA',
+                backgroundColor: '#251313',
+                data: [10, 0, 338]
+                },
+                {
+                label: 'TILCARA',
+                backgroundColor: '#f87979',
+                data: [200, 0, 0]
+                },
+                {
+                label: 'SY 330',
+                backgroundColor: '#805050',
+                data: [0, 0, 110]
+                },
+                {
+                label: 'SAETA',
+                backgroundColor: '#485379',
+                data: [0, 90, 0]
+                },
+                {
+                label: '75 ANIVERSARIO',
+                backgroundColor: '#f87979',
+                data: [71, 0, 0]
+                },
+                {
+                label: 'BELLACO',
+                backgroundColor: '#f01313',
+                data: [0, 52, 0]
+                },
+                {
+                label: 'CHACARERO',
+                backgroundColor: '#79f879',
+                data: [30, 0, 0]
+                },
             ]
         },
         options: {
