c94a1c756008bf69a626a2f1cbf31eda5e554659
lucashrossi <lucashrossi@gmail.com>
2018-10-22 14:44:41 -0300

    PF

--- components/MIXCO.vue
+++ components/MIXCO.vue
@@ -7,29 +7,72 @@ export default {
     data () {
         return {
         datacollection: {
-            labels: ['SY 110', 'Pleno', '55 CL2', 'SY 300', 'Claraz', 'Destello', 'SY 200', 'SY 211', 'Saeta', 'Camba', 'Bellaco', 'Meteoro', 'SY 330', 'SY 120', 'Total Año'],
+            labels: ['SY 110', 'Pleno', '55 CL2', 'SY 300', 'Claraz', 'Destello', 'SY 200', 'SY 211', 'Saeta', 'Camba', 'Bellaco', 'Meteoro', 'SY 330', 'SY 120', 'Totales'],
             datasets: [{
                 label: '2018',
-                backgroundColor: ['rgba(170, 150, 120, 1)', 'rgba(200, 88, 88, 1)', 'rgba(194, 82, 80, 1)', 'rgba(40, 194, 184, 1)', 'rgba(225, 200, 185, 1)', 'rgba(20, 50, 0, 1)', 'rgba(185, 250, 85, 1)', 'rgba(196, 88, 80, 1)', 'rgba(17, 17, 185, 1)', 'rgba(136, 186, 147, 1)', 'rgba(150, 10, 0, 1)', 'rgba(140, 186, 47, 1)', 'rgba(50, 200, 0, 1)', 'rgba(255, 0, 0, 1)', 'rgba(150, 150, 150, 1)'],
-                data: [0, 172, 355, 185, 455, 1696, 616, 1745, 2839, 748, 1086, 3070, 5336, 9080, 27383],
+                backgroundColor: ['rgba(170, 150, 120, 1)', 'rgba(200, 88, 88, 1)', 'rgba(194, 82, 80, 1)', 'rgba(40, 194, 184, 1)', 'rgba(225, 200, 185, 1)', 'rgba(20, 50, 0, 1)', 'rgba(185, 250, 85, 1)', 'rgba(196, 88, 80, 1)', 'rgba(17, 17, 185, 1)', 'rgba(136, 186, 147, 1)', 'rgba(150, 10, 0, 1)', 'rgba(140, 186, 47, 1)', 'rgba(50, 200, 0, 1)', 'rgba(255, 0, 0, 1)'],
+                data: [0, 172, 355, 185, 455, 1696, 616, 1745, 2839, 748, 1086, 3070, 5336, 9080],
                 yAxisID: 'left-y-axis'
                 },
                 {
                 label: '2017',
                 hidden: true,
-                backgroundColor: ['rgba(170, 150, 120, 0.4)', 'rgba(200, 88, 88, 0.4)', 'rgba(194, 82, 80, 0.4)', 'rgba(40, 194, 184, 0.4)', 'rgba(225, 200, 185, 0.4)', 'rgba(20, 50, 0, 0.4)', 'rgba(185, 250, 85, 0.4)', 'rgba(196, 88, 80, 0.4)', 'rgba(17, 17, 185, 0.4)', 'rgba(136, 186, 147, 0.4)', 'rgba(150, 10, 0, 0.4)', 'rgba(140, 186, 47, 0.4)', 'rgba(50, 200, 0, 0.4)', 'rgba(255, 0, 0, 0.4)', 'rgba(150, 150, 150, 0.4)'],
-                data: [0, 0, 100, 1411, 1196, 0, 1101, 350, 1280, 3807, 3855, 2122, 739, 30, 15991],
+                backgroundColor: ['rgba(170, 150, 120, 0.4)', 'rgba(200, 88, 88, 0.4)', 'rgba(194, 82, 80, 0.4)', 'rgba(40, 194, 184, 0.4)', 'rgba(225, 200, 185, 0.4)', 'rgba(20, 50, 0, 0.4)', 'rgba(185, 250, 85, 0.4)', 'rgba(196, 88, 80, 0.4)', 'rgba(17, 17, 185, 0.4)', 'rgba(136, 186, 147, 0.4)', 'rgba(150, 10, 0, 0.4)', 'rgba(140, 186, 47, 0.4)', 'rgba(50, 200, 0, 0.4)', 'rgba(255, 0, 0, 0.4)'],
+                data: [0, 0, 100, 1411, 1196, 0, 1101, 350, 1280, 3807, 3855, 2122, 739, 30],
                 yAxisID: 'left-y-axis'
                 }, 
-                // {
-                // label: 'Diferencia Años',
-                
-                // data: [81, 90, -2, 77, 75, 39, 100, 100],
+                {
+                label: 'Total Años',
+                hidden: true,
+                data: [0, 172, 455, 1596, 1651, 1696, 1717, 2095, 4119, 4555, 4941, 5192, 6075, 9110],
 
-                // // Changes this dataset to become a line
-                // type: 'line',
-                // yAxisID: 'right-y-axis'
-                // }
+                // Changes this dataset to become a line
+                type: 'line',
+                // yAxisID: 'right-y-axis',
+                fill: false
+                }, 
+                {
+                label: 'Total 2018',
+                
+                data: [, , , , , , , , , , , , , , 27383],
+
+                // Changes this dataset to become a line
+                type: 'line',
+                yAxisID: 'right-y-axis',
+                fill: false,
+                showLine: false,
+                pointRadius: 5,
+                // pointBackgroundColor: 'rgba(255, 255, 255, 1)',
+                borderColor: 'rgba(255, 0, 0, 1)'
+                }, 
+                {
+                label: 'Total 2017',
+                
+                data: [, , , , , , , , , , , , , , 15991],
+
+                // Changes this dataset to become a line
+                type: 'line',
+                yAxisID: 'right-y-axis',
+                fill: false,
+                showLine: false,
+                pointRadius: 5,
+                // pointBackgroundColor: 'rgba(0, 0, 0, 1)',
+                borderColor: 'rgba(0, 255, 0, 1)'
+                }, 
+                {
+                label: 'Total',
+                
+                data: [, , , , , , , , , , , , , , 43374],
+
+                // Changes this dataset to become a line
+                type: 'line',
+                yAxisID: 'right-y-axis',
+                fill: false,
+                showLine: false,
+                pointRadius: 5,
+                // pointBackgroundColor: 'rgba(0, 0, 0, 1)',
+                borderColor: 'rgba(0, 0, 255, 1)'
+                }
                 ],
             // labels: ['I', 'IIN', 'IIS', 'III', 'IV', 'VN', 'VS', 'NOA']
         },
@@ -52,23 +95,23 @@ export default {
                         beginAtZero: true,
                         // steps: 10,
                         // stepValue: 5,
-                        max: 60000,
+                        max: 16000,
                         min: 0
-                    }
-    
+                    },
+                    // stacked: true
                 }, 
-                // {
-                //     id: 'right-y-axis',
-                //     type: 'linear',
-                //     position: 'right',
-                //     ticks: {
-                //         beginAtZero: true,
-                //         // steps: 10,
-                //         // stepValue: 5,
-                //         max: 150,
-                //         min: -70
-                //     }
-                // }
+                {
+                    id: 'right-y-axis',
+                    type: 'linear',
+                    position: 'right',
+                    ticks: {
+                        beginAtZero: true,
+                        // steps: 10,
+                        // stepValue: 5,
+                        max: 80000,
+                        min: 0
+                    },
+                }
                 ],
                 xAxes: [{ 
                 scaleLabel: {
--- components/MIXCP.vue
+++ components/MIXCP.vue
@@ -7,29 +7,72 @@ export default {
     data () {
         return {
         datacollection: {
-            labels: ['Pleno', '55 CL2', 'Destello', 'Camba', 'SY 110', 'SY 300', 'Claraz', 'SY 200', 'SY 120', 'Saeta', 'Bellaco', 'Meteoro', 'SY 211', 'SY 330', 'Total Año'],
+            labels: ['Pleno', '55 CL2', 'Destello', 'Camba', 'SY 110', 'SY 300', 'Claraz', 'SY 200', 'SY 120', 'Saeta', 'Bellaco', 'Meteoro', 'SY 211', 'SY 330', 'Totales'],
             datasets: [{
                 label: '2018',
-                backgroundColor: ['rgba(200, 88, 88, 1)', 'rgba(194, 82, 80, 1)', 'rgba(20, 50, 0, 1)', 'rgba(136, 186, 147, 1)', 'rgba(170, 150, 120, 1)', 'rgba(40, 194, 184, 1)', 'rgba(225, 200, 185, 1)', 'rgba(185, 250, 85, 1)', 'rgba(255, 0, 0, 1)', 'rgba(17, 17, 185, 1)', 'rgba(150, 10, 0, 1)', 'rgba(140, 186, 47, 1)', 'rgba(196, 88, 80, 1)', 'rgba(50, 200, 0, 1)', 'rgba(150, 150, 150, 1)'],
-                data: [0, 0, 0, 0, 0, 2538, 2242, 3515, 4125, 8242, 3664, 7680, 10774, 14103, 56883],
+                backgroundColor: ['rgba(200, 88, 88, 1)', 'rgba(194, 82, 80, 1)', 'rgba(20, 50, 0, 1)', 'rgba(136, 186, 147, 1)', 'rgba(170, 150, 120, 1)', 'rgba(40, 194, 184, 1)', 'rgba(225, 200, 185, 1)', 'rgba(185, 250, 85, 1)', 'rgba(255, 0, 0, 1)', 'rgba(17, 17, 185, 1)', 'rgba(150, 10, 0, 1)', 'rgba(140, 186, 47, 1)', 'rgba(196, 88, 80, 1)', 'rgba(50, 200, 0, 1)'],
+                data: [0, 0, 0, 0, 0, 2538, 2242, 3515, 4125, 8242, 3664, 7680, 10774, 14103],
                 yAxisID: 'left-y-axis'
                 },
                 {
                 label: '2017',
                 hidden: true,
-                backgroundColor: ['rgba(200, 88, 88, 0.4)', 'rgba(194, 82, 80, 0.4)', 'rgba(20, 50, 0, 0.4)', 'rgba(136, 186, 147, 0.4)', 'rgba(170, 150, 120, 0.4)', 'rgba(40, 194, 184, 0.4)', 'rgba(225, 200, 185, 0.4)', 'rgba(185, 250, 85, 0.4)', 'rgba(255, 0, 0, 0.4)', 'rgba(17, 17, 185, 0.4)', 'rgba(150, 10, 0, 0.4)', 'rgba(140, 186, 47, 0.4)', 'rgba(196, 88, 80, 0.4)', 'rgba(50, 200, 0, 0.4)', 'rgba(150, 150, 150, 0.4)'],
-                data: [0, 0, 0, 0, 700, 0, 1546, 461, 0, 620, 5520, 3244, 1116, 790, 13997],
+                backgroundColor: ['rgba(200, 88, 88, 0.4)', 'rgba(194, 82, 80, 0.4)', 'rgba(20, 50, 0, 0.4)', 'rgba(136, 186, 147, 0.4)', 'rgba(170, 150, 120, 0.4)', 'rgba(40, 194, 184, 0.4)', 'rgba(225, 200, 185, 0.4)', 'rgba(185, 250, 85, 0.4)', 'rgba(255, 0, 0, 0.4)', 'rgba(17, 17, 185, 0.4)', 'rgba(150, 10, 0, 0.4)', 'rgba(140, 186, 47, 0.4)', 'rgba(196, 88, 80, 0.4)', 'rgba(50, 200, 0, 0.4)'],
+                data: [0, 0, 0, 0, 700, 0, 1546, 461, 0, 620, 5520, 3244, 1116, 790],
                 yAxisID: 'left-y-axis'
                 }, 
-                // {
-                // label: 'Diferencia Años',
-                
-                // data: [81, 90, -2, 77, 75, 39, 100, 100],
+                {
+                label: 'Total Años',
+                hidden: true,
+                data: [0, 0, 0, 0, 700, 2538, 3788, 3976, 4125, 8862, 9184, 10924, 11890, 14893],
 
-                // // Changes this dataset to become a line
-                // type: 'line',
-                // yAxisID: 'right-y-axis'
-                // }
+                // Changes this dataset to become a line
+                type: 'line',
+                // yAxisID: 'right-y-axis',
+                fill: false
+                },
+                {
+                label: 'Total 2018',
+                
+                data: [, , , , , , , , , , , , , , 56883],
+
+                // Changes this dataset to become a line
+                type: 'line',
+                yAxisID: 'right-y-axis',
+                fill: false,
+                showLine: false,
+                pointRadius: 5,
+                // pointBackgroundColor: 'rgba(255, 255, 255, 1)',
+                borderColor: 'rgba(255, 0, 0, 1)'
+                }, 
+                {
+                label: 'Total 2017',
+                
+                data: [, , , , , , , , , , , , , , 13997],
+
+                // Changes this dataset to become a line
+                type: 'line',
+                yAxisID: 'right-y-axis',
+                fill: false,
+                showLine: false,
+                pointRadius: 5,
+                // pointBackgroundColor: 'rgba(0, 0, 0, 1)',
+                borderColor: 'rgba(0, 255, 0, 1)'
+                }, 
+                {
+                label: 'Total',
+                
+                data: [, , , , , , , , , , , , , , 70880],
+
+                // Changes this dataset to become a line
+                type: 'line',
+                yAxisID: 'right-y-axis',
+                fill: false,
+                showLine: false,
+                pointRadius: 5,
+                // pointBackgroundColor: 'rgba(0, 0, 0, 1)',
+                borderColor: 'rgba(0, 0, 255, 1)'
+                }
                 ],
             // labels: ['I', 'IIN', 'IIS', 'III', 'IV', 'VN', 'VS', 'NOA']
         },
@@ -52,23 +95,23 @@ export default {
                         beginAtZero: true,
                         // steps: 10,
                         // stepValue: 5,
-                        max: 60000,
+                        max: 16000,
                         min: 0
                     }
     
                 }, 
-                // {
-                //     id: 'right-y-axis',
-                //     type: 'linear',
-                //     position: 'right',
-                //     ticks: {
-                //         beginAtZero: true,
-                //         // steps: 10,
-                //         // stepValue: 5,
-                //         max: 150,
-                //         min: -70
-                //     }
-                // }
+                {
+                    id: 'right-y-axis',
+                    type: 'linear',
+                    position: 'right',
+                    ticks: {
+                        beginAtZero: true,
+                        // steps: 10,
+                        // stepValue: 5,
+                        max: 80000,
+                        min: 0
+                    },
+                }
                 ],
                 xAxes: [{ 
                 scaleLabel: {
