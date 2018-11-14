9427b78251d7bc926043c5060c111f8d9c0470df
lucashrossi <lucashrossi@gmail.com>
2018-11-14 16:30:43 -0300

    PF

--- pages/DashB/index.vue
+++ pages/DashB/index.vue
@@ -156,8 +156,10 @@
           </v-flex>
           <v-spacer></v-spacer> 
           <v-flex xs12 sm10 offset-sm1 md6 offset-md0 lg3>            
-            <iframe src="http://semillasbuck.com.ar/noticias"  width="100%" height="400px" frameborder="0"></iframe>
-            <!-- <embed class="elevation-20" src="http://semillasbuck.com.ar/noticias/" width="100%" height="400px"> -->
+            <!-- <iframe src="http://semillasbuck.com.ar/noticias"  width="100%" height="400px" frameborder="0"></iframe> -->
+            <embed src="http://semillasbuck.com.ar" width="100%" height="400px">
+            <!-- <embed src="https://www.infobae.com/" width="100%" height="400px"> -->
+            <!--  -->
 
           </v-flex>
         </v-layout>
